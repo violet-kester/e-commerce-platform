9d3e89d5f45aa32cdefa4e129313d3203d43524c
Violet Kester <kestervj89@gmail.com>
2023-08-26 13:25:06 -0400

    create a stripe customer when a user signs up

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import Stripe from 'stripe';
+import { userInfo } from "os";
 
 const prisma = new PrismaClient();
 
@@ -13,4 +15,24 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  events: {
+    // create a stripe customer when a user signs up
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: '2022-11-15'
+      });
+      // create a stripe customer
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+        // add stripe customer id to prisma user
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id }
+        });
+      }
+    }
+  }
 });
