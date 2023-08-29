9c14a53570179545f58762481371de07b0a59227
Violet Kester <kestervj89@gmail.com>
2023-08-29 18:37:32 -0400

    formatted prices, added product type

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image';
+import formatPrice from '@/util/PriceFormat';
+import { ProductType } from '@/types/ProductType';
 
-export default function Product({ name, image, price }) {
+export default function Product({
+    name,
+    image,
+    price
+}: ProductType) {
     return (
         <div>
             <Image src={image} alt={name} width={300} height={600} />
             <h1>{name}</h1>
-            <p>{price}</p> {/* needs to be formatted */}
+            <p>{price ? formatPrice(price) : 'N/A'}</p>
         </div>
     );
 }
