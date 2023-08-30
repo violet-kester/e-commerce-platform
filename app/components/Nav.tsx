8e2939e7f46685945c272a0a324ff503137c19d9
Violet Kester <kestervj89@gmail.com>
2023-08-29 22:02:34 -0400

    modified placeholder logo

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function Nav({ user }: Session) {
     return (
         <nav className='flex justify-between items-center py-8'>
-            <h1>Styled</h1>
+            <h1>E-commerce Platform</h1>
             <ul className='flex items-center gap-12'>
                 {!user && (
                     <li className='bg-teal-600 text-white py-2 px-4 rounded-md'>
