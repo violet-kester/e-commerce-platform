10d30dfdd0a1189bdf56ade0642a195bd7c52de6
Violet Kester <kestervj89@gmail.com>
2023-08-29 22:02:18 -0400

    added grid, updated doc strings

--- app/page.tsx
+++ app/page.tsx
@@ -42,13 +42,13 @@ const getProducts = async () => {
   const productWithPrices = await Promise.all(
     products.data.map(async (product) => {
       const prices = await stripe.prices.list({ product: product.id });
-      return{
+      return {
         id: product.id,
         name: product.name,
         price: prices.data[0].unit_amount,
         image: product.images[0],
         currency: prices.data[0].currency
-      }
+      };
     })
   );
 
@@ -62,28 +62,30 @@ export default async function Home() {
   //     id: 'prod_OXFrVE1QwzF85H',
   //     name: 'Bold Kaftan',
   //     price: 15000,
-  //     image: 'https://files.stripe.com/links/MDB8YWNjdF8xTmgyMlBDMG4xYUIyVWh2fGZsX2xpdmVfbVkzSmNtWW9Vd0taNFRqR0NzR1pXRDRh00QKGgTA9Y',
+  //     image: 'https://files.stripe.com/links/MDB8YWN...',
   //     currency: 'usd'
   //   },
   //   {
   //     id: 'prod_OXFqIxgYSaNxJl',
   //     name: 'Concentric Kaftan',
   //     price: 14000,
-  //     image: 'https://files.stripe.com/links/MDB8YWNjdF8xTmgyMlBDMG4xYUIyVWh2fGZsX2xpdmVfQ3Axalg4YXN0WUI4cFRRWmF6OHFVVG9t000FdBbBh8',
+  //     image: 'https://files.stripe.com/links/MDB8YWN...',
   //     currency: 'usd'
   //   },
   //   {
   //     id: 'prod_OXFpT49jHkxmgz',
   //     name: 'Striped Kaftan',
   //     price: 12000,
-  //     image: 'https://files.stripe.com/links/MDB8YWNjdF8xTmgyMlBDMG4xYUIyVWh2fGZsX2xpdmVfTnRoMWpkWmZSWGlvbzhBTFZYOWtMMnFk00RAqaMZvh',
+  //     image: 'https://files.stripe.com/links/MDB8YWN...',
   //     currency: 'usd'
   //   }
   // ]
   return (
-    <main>
-      {/* pass product properties to product component via props */}
-      {products.map(product => <Product {...product} />)}
+    <main className='grid grid-cols-fluid gap-12'>
+
+      {/* passes fetched product data to product component via props */}
+      {products.map(product => <Product {...product} key={product.id} />)}
+
     </main>
   );
 }
