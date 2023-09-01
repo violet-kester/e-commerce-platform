97d38cf5ff040b4fb35642e4e5aec9320ace0bd2
Violet Kester <kestervj89@gmail.com>
2023-09-01 01:01:18 -0400

    change logo, favicon

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -8,7 +8,10 @@ import Link from 'next/link';
 export default function Nav({ user }: Session) {
     return (
         <nav className='flex justify-between items-center py-8'>
-            <Link href='/'><h1>E-commerce Platform</h1></Link>
+            <Link href='/' className='flex items-center gap-4'>
+                <Image src='/logo.png' alt='Logo' width={48} height={48} />
+                <h1>E-commerce Platform</h1>
+            </Link>
             <ul className='flex items-center gap-12'>
                 {!user && (
                     <li className='bg-teal-600 text-white py-2 px-4 rounded-md'>
