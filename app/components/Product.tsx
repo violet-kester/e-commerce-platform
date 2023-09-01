d1cf6672769c4a84a03db220acaf64445371c9f6
Violet Kester <kestervj89@gmail.com>
2023-08-31 22:47:34 -0400

    added link to product description page

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,19 +1,30 @@
 import Image from 'next/image';
 import formatPrice from '@/util/PriceFormat';
 import { ProductType } from '@/types/ProductType';
+import Link from 'next/link';
 
 export default function Product({
     name,
     image,
-    price
+    price,
+    description,
+    id
 }: ProductType) {
     return (
-        <div className='text-gray-700'>
-            <Image src={image} alt={name} width={600} height={900} className='rounded-lg' />
-            <div className='font-medium py-2'>
-                <h1>{name}</h1>
-                <h2 className='text-sm text-teal-700'>{price ? formatPrice(price) : 'Price unavailable'}</h2>
+        // avoid re-fetching product data from stripe by querying for product data within link
+        <Link href={{
+            pathname: `/products/${id}`,
+            query: { name, image, price, description, id }
+        }}>
+            <div className='text-gray-700'>
+                <Image src={image} alt={name} width={600} height={900} className='rounded-lg' />
+                <div className='font-medium py-2'>
+                    <h1>{name}</h1>
+                    <h2 className='text-sm text-teal-700'>
+                        {price ? formatPrice(price) : 'Price unavailable'}
+                    </h2>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
