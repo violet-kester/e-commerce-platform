ee33431c217603d817c281693bb57ce91a60cfbb
Violet Kester <kestervj89@gmail.com>
2023-09-03 21:17:35 -0400

    added toggling, add products to cart store

--- app/store.ts
+++ app/store.ts
@@ -1,25 +1,43 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware'; // syncs up local storage
-
-type CartItem = {
-  id: string,
-  name: string,
-  images?: string[],
-  description?: string,
-  unit_amount: number,
-  quantity: number,
-};
+import { AddCartType } from '@/types/AddCartType';
 
 type CartState = {
-  cart: CartItem[],
+  cart: AddCartType[],
   isOpen: boolean,
+  toggleCart: () => void,
+  // clearCart: () => void,
+  addProduct: (item: AddCartType) => void,
+  // removeProduct: (item: AddCartType) => void,
+
 };
 
 export const useCartStore = create<CartState>()(
   persist(
-    (set) => ({
+    (set) => ({ // set used to update data in state
       cart: [],
       isOpen: false,
+      toggleCart: () => set((state) => ({ // state = cart store
+        isOpen: !state.isOpen
+      })),
+      addProduct: (item) => set((state) => {
+        // check for items already in cart
+        const existingItem = state.cart.find(cartItem => cartItem.id === item.id);
+        // if item already exists, increase quantity
+        if (existingItem) {
+          const updatedCart = state.cart.map((cartItem) => {
+            if (cartItem.id === item.id) {
+              return { ...cartItem, quantity: cartItem.quantity + 1 };
+            }
+            return cartItem;
+          });
+          // return updated cart
+          return { cart: updatedCart };
+        } else {
+          // if item doesn't exist, add it to cart
+          return { cart: [...state.cart, { ...item, quantity: 1 }] };
+        }
+      }),
     }),
     { name: 'cart-store' }
   )
