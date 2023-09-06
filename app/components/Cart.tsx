ffc429c6058b7883e33c77767291a79762417937
Violet Kester <kestervj89@gmail.com>
2023-09-05 22:58:47 -0400

    fleshing out shopping cart ui component

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -2,12 +2,41 @@
 
 import Image from 'next/image';
 import { useCartStore } from '../store';
+import formatPrice from '@/util/PriceFormat';
 
 export default function Cart() {
     const cartStore = useCartStore();
     return (
-        <div>
-            <h1>Cart</h1>
+        <div // dimmed bg
+            onClick={() => cartStore.toggleCart()} // closes cart when dimmed bg is clicked
+            className='fixed w-full h-screen left-0 top-0 bg-black/25'
+        >
+            <div // cart items panel
+                onClick={(e) => e.stopPropagation} // prevents cart from closing when clicked
+                className='bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700'>
+                <h1>Here's your shopping list.</h1>
+                {cartStore.cart.map((item) => (
+                    <div className="flex py-4 gap-4">
+                        <Image
+                            className='rounded-md'
+                            src={item.image}
+                            alt={item.name}
+                            width={120}
+                            height={120} />
+                        <div>
+                            <h2>{item.name}</h2>
+                            <h2>Quantity: {item.quantity}</h2>
+                            <p className="text-sm">
+                                {item.unit_amount && formatPrice(item.unit_amount)}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+                <button className='py-2 mt-4 bg-teal-700 w-full rounded-md text-white'>
+                    Checkout
+                </button>
+            </div>
+
         </div>
     );
 }
